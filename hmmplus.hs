960df0928b4d16219f65d1a090d7b7a3af84d2a7
Noah Daniels <ndaniels@mac.com>
2011-09-08 14:47:49 -0400

    initial import for github

---
+++ hmmplus.hs
@@ -0,0 +1,252 @@
+{-# LANGUAGE TypeSynonymInstances, TemplateHaskell, QuasiQuotes, 
+             MultiParamTypeClasses, FlexibleInstances, UndecidableInstances,
+             DeriveDataTypeable, ScopedTypeVariables, NamedFieldPuns #-}
+
+module Smurf.HmmPlus where
+import Language.Pads.Padsc
+import Language.Pads.GenPretty
+import Control.Monad
+import System.IO.Unsafe (unsafePerformIO)
+
+ws = REd "[\t ]+|$" " "
+
+amino = "ACDEFGHIKLMNPQRSTVWY"
+nucleotide = "ACTG"
+
+[pads|
+  data SmurfFile = SmurfFile { header::SmurfHeader, hmm::HMM <| (getAlphabet header, getNumNodes header) |> }
+  
+  type SmurfHeader = [Line HeaderLine] terminator Try (LitRE 'HMM ')
+  
+  data HeaderLine = HeaderLine { tag::Tag, ws, payload::Payload tag }
+  
+  data Tag = FileVersion "HMMER3/a" -- this string literal will change with major file version changes
+            | NAME | ACC | DESC | LENG | ALPH | RF | CS | MAP | DATE | MEAN | RMSD
+            | COM | NSEQ | EFFN | CKSUM | GA | TC | NC | STATS | BETA | Other (StringSE ws)
+  
+  data Payload (t::Tag) = case t of
+      FileVersion -> Version VersionString
+    | NAME -> Name StringLn
+    | ACC -> Accession StringLn
+    | DESC -> Description StringLn
+    | LENG -> ModelLength Int
+    | ALPH -> Alphabet StringLn
+    | RF -> ReferenceAnnotation StringLn
+    | CS -> ConsensusStructure StringLn
+    | MAP -> MapAnnotation StringLn
+    | DATE -> Date StringLn
+    | COM -> CommandLog StringLn
+    | NSEQ -> SequenceNumber Int
+    | EFFN -> EffectiveSeq Double
+    | CKSUM -> Checksum Int
+    | GA -> PfamGathering (Double, ws, Double)
+    | TC -> PfamTrusted (Double, ws, Double)
+    | NC -> PfamNoise (Double, ws, Double)
+    | STATS -> Stats {"LOCAL", ws, scoredist::ScoreDistribution, ws, values::[Double | ws] terminator Try EOR }
+    | BETA -> Beta StrandPair
+    | Other tag -> BadTag StringLn
+    | otherwise -> OtherTag StringLn
+    
+  type VersionString = StringLn
+  
+  data ScoreDistribution = VLAMBDA | VMU | FTAU | MSV | VITERBI | FORWARD
+  
+   -- firstRes secondRes Length maxGap parallelism exposure
+  data StrandPair = StrandPair {
+        firstStart :: Int, ' ',
+        secondStart :: Int, ' ',
+        pairLength :: Int, ' ',
+        maxGap :: Int, ' ',
+        parallel :: Direction, ' ',
+        exposure :: ExposureList
+  }
+    
+  type ExposureList = [ Exposure ] terminator Try EOR
+  data Exposure = In 'i'
+                | Out 'o'  
+                
+  data Direction = Parallel "1"
+                 | Antiparallel "-1"
+  
+  data HMM (alphabet::String, numNodes::Int) = HMM {
+    "HMM", ws, hmmAlphabet::[Letter alphabet | ws] length <| length alphabet |>, ws, EOR,
+    ws, transitionHeader::TransitionDescription, EOR,
+    composition::Maybe (ws, "COMPO", ws, EmissionProbabilities alphabet, ws, EOR),
+    insertZeroEmissions::InsertEmissions alphabet,
+    stateZeroTransitions::StateTransitions,
+    nodes::[HmmNode <| alphabet |>] terminator "//" where <| numNodes == length nodes |> }
+--    rest::[StringLn | EOR] terminator EOF }
+              
+  type Letter (alphabet::String) = constrain c::Char where <| c `elem` alphabet |>
+  
+  type EmissionProbabilities (alphabet::String) = [ Double | ws ] length <| length alphabet |> 
+  
+--  type TransitionProbabilities (numStates::Int) = [ LogProbability | ws ] length numStates
+
+  -- data TransitionProbabilities = TransitionProbabilities {
+  --     m_m :: TransitionProbability <|(Match, Match)|>, ws,
+  --     m_i :: TransitionProbability <|(Match, Insertion)|>, ws,
+  --     m_d :: TransitionProbability <|(Match, Deletion)|>, ws,
+  --     i_m :: TransitionProbability <|(Insertion, Match)|>, ws,
+  --     i_i :: TransitionProbability <|(Insertion, Insertion)|>, ws,
+  --     d_m :: TransitionProbability <|(Deletion, Match)|>, ws,
+  --     d_d :: TransitionProbability <|(Deletion, Deletion)|>
+  -- }
+  
+  data TransitionProbabilities = TransitionProbabilities {
+      m_m :: TransitionProbability <|(0, 0)|>, ws,
+      m_i :: TransitionProbability <|(0, 1)|>, ws,
+      m_d :: TransitionProbability <|(0, 2)|>, ws,
+      i_m :: TransitionProbability <|(1, 0)|>, ws,
+      i_i :: TransitionProbability <|(1, 1)|>, ws,
+      d_m :: TransitionProbability <|(2, 0)|>, ws,
+      d_d :: TransitionProbability <|(2, 2)|>
+  }
+  
+  type TransitionDescription = [ StringSE ws | ws] terminator (Try EOR)
+  
+  type InsertEmissions (alphabet::String) = (ws, EmissionProbabilities alphabet, ws, EOR)
+  
+  type StateTransitions = (ws, TransitionProbabilities, ws, EOR)
+  
+  data HmmNode (alphabet::String) = HmmNode {
+                ws, nodeNum::Int, ws, matchEmissions::EmissionProbabilities alphabet, ws, annotations::EmissionAnnotationSet, EOR,
+                insertionEmissions::InsertEmissions alphabet,
+                transitions::StateTransitions
+  }
+  
+  
+  type EmissionAnnotationSet = (EmissionAnnotation, ws, EmissionAnnotation, ws, EmissionAnnotation)
+  
+  data EmissionAnnotation = MAPA Int
+                          | Unused '-'
+                          | RForCS Char
+                          
+
+  data LogProbability = LogZero '*'
+                      | NonZero Double
+                      
+  data TransitionProbability (fState::HMMState, tState::HMMState) = TransitionProbability { 
+                                                                  logProbability::LogProbability,
+                                                                  fromState = value fState::HMMState,
+                                                                  toState = value tState::HMMState
+  
+  }
+
+    
+  -- data HMMState = Match | Insertion | Deletion
+  type HMMState = Int
+                      
+-- do we want these to be types or newtypes? newtype enforces type checking
+-- but might prove cumbersome in the algorithm.
+-- consider just making these type aliases.        
+-- is there a way to make these newtypes but declare a mutual conversion so we can compare them?
+-- we will want to take the max of several of these when we implement forward algorithm
+  newtype FromMatch = FromMatch LogProbability
+  type MatchToMatch = FromMatch
+  type MatchToInsertion = FromMatch
+  type MatchToDeletion = FromMatch
+  newtype FromInsertion = FromInsertion LogProbability
+  type InsertionToMatch = FromInsertion
+  type InsertionToInsertion = FromInsertion
+  newtype FromDeletion = FromDeletion LogProbability
+  type DeletionToMatch = FromDeletion
+  type DeletionToDeletion = FromDeletion
+
+|]
+
+
+-- instance Eq LogProbability where
+--   x == y = case (x, y) of
+--             (LogZero, LogZero) -> True
+--             (NonZero x', NonZero y') -> x' == y'
+--             (_, _) -> False
+
+-- instance Ord LogProbability where
+--   x < y = case (x, y) of
+--             (LogZero, LogZero) -> False
+--             (LogZero, _) -> True
+--             (NonZero _, LogZero) -> False
+--             (NonZero x', NonZero y') -> x' < y'
+
+
+-- MatchToMatch < MatchToInsertion
+-- lt :: MatchToMatch -> MatchToInsertion -> Bool
+-- lt (MatchToMatch mp) (MatchToInsertion mp') = mp < mp'
+
+
+
+
+-- I think we'll want a max function on a TransitionProbabilities, which gives us the max of the 7
+
+getAlphabet :: SmurfHeader -> String
+getAlphabet ((HeaderLine {tag, payload}):xs) = case tag of
+                    ALPH -> case payload of
+                              Alphabet "amino" -> amino
+                              Alphabet "nucleotide" -> nucleotide
+                              otherwise -> error "Invalid alphabet"
+                    otherwise -> getAlphabet xs
+
+getNumNodes :: SmurfHeader -> Int
+-- getNumNodes header = 343 -- replace this  
+getNumNodes ((HeaderLine {tag, payload}):xs) = case tag of
+                    LENG -> case payload of
+                              ModelLength i -> i
+                              otherwise -> error "Invalid model length"
+                    otherwise -> getNumNodes xs
+                    
+-- given a SmurfHeader, return a list of beta strand pairs, possibly empty.                    
+getBetaPairs :: SmurfHeader -> [StrandPair]
+getBetaPairs ((HeaderLine {tag, payload}):xs) = case tag of
+                    BETA -> case payload of 
+                              Beta b -> (b:getBetaPairs xs)
+                              otherwise -> error "Invalid beta"
+                    otherwise -> getBetaPairs xs                    
+getBetaPairs [] = []                
+
+getBetaStrands :: SmurfHeader -> [BetaStrand]
+getBetaStrands h = getBetaStrands' 1 (getBetaPairs h)
+  where getBetaStrands' _ [] = []
+        getBetaStrands' counter (x:xs) = createBetas counter x ++ getBetaStrands' (counter + 2) xs
+          where createBetas counter x = ((BetaStrand counter start1 (start1 + pairLength x) (exposure x)) : [(BetaStrand (counter + 1) start2 (start2 + pairLength x) (flipExposures $ exposure x))])
+                  where start1 = firstStart x
+                        start2 = secondStart x
+                        -- start2 depends on parallel
+                        -- start2 = secondStart 1 (really??)
+                        -- check logic for this
+                        -- I think this depends on parallel vs antiparallel
+                        -- should not flip exposures, just reverse the list if antiparallel
+                        -- need a way to represent paired residues (position, pairedpos)
+                        flipExposures ys = map flipExposure ys
+                          where flipExposure In = Out
+                                flipExposure Out = In
+
+-- rethink this. A BetaStrand is a list of (Position, PairedPosition, Exposure) tuples.
+-- Order can be derived from position of first residue.
+-- data BetaStrand = {betaStrandId::Int, betaStrandPositions::[BetaResidue]}
+-- still have to handle multiply paired....? pairedForward, pairedBack???
+-- data BetaResidue = {position::Int, pairedStrand::Int, pairedPosition::Int, solventExposure::Exposure}
+-- we want pairedPos to be relative to a specific residue in a paired strand
+-- this will allow flexibility with gaps in non-beta regions!
+
+data BetaResidue = {strandPosition::Int, pairedStrand::Int, pairedPosition::Int, solventExposure::exposure}
+
+data BetaStrand = 
+
+
+data BetaStrand = BetaStrand {order::Int, startPosition::Int, endPosition::Int, exposureList::[Exposure]}
+
+    
+
+getTag :: Tag -> SmurfHeader -> Payload
+getTag t ((HeaderLine {tag, payload}):xs) = if tag == t
+                                              then payload
+                                            else
+                                              getTag t xs
+getTag t [] = error "Tag not found"  
+                  
+
+result = do
+        { (SmurfFile header hmm, md) <- parseFile "test.hmm+"
+        ; return (header, hmm, md)
+        }
---
+++ psipred.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE TypeSynonymInstances, TemplateHaskell, QuasiQuotes, 
+             MultiParamTypeClasses, FlexibleInstances, UndecidableInstances,
+             DeriveDataTypeable, ScopedTypeVariables, NamedFieldPuns #-}
+
+module Smurf.PsiPred where
+import Language.Pads.Padsc
+import Language.Pads.GenPretty
+import Control.Monad
+import System.IO.Unsafe (unsafePerformIO)
+
+ws = REd "[\t ]+|$" " "
+
+amino = "ACDEFGHIKLMNPQRSTVWY"
+
+[pads|
+  data PsiPredFile = PsiPredFile { header::PsiPredHeader, ws, ss_predictions::[Line SSPrediction] terminator EOF }
+  
+  type PsiPredHeader = ('#', ws, StringLn, EOR)
+  
+  data SSPrediction = SSPrediction { ws,
+                      residueNum::Int, ws,
+                      residueLetter::Letter, ws,
+                      ss_prediction::SecondaryStructure, ws,
+                      turn_score::Double, ws,
+                      alpha_score::Double, ws,
+                      beta_score::Double
+  }
+  
+  data SecondaryStructure = Alpha 'H'
+                          | Beta 'E'
+                          | Turn 'C'
+                          
+  type Letter = constrain c::Char where <| c `elem` amino |>                        
+
+|]
+
+result = do
+        { (PsiPredFile header ss_predictions, md) <- parseFile "test.ss2"
+        ; return (header, ss_predictions, md)
+        }
