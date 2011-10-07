c6e20c4f53ed686666dbb5f4d41a75b89650fa56
Andrew Gallant (Cheetah) <Andrew.Gallant@tufts.edu>
2011-10-07 14:54:10 -0400

    DEPENDENCIES keeps track of what smurf2 needs to compile. Main.hs has command line processing and initializes beta residue collection. README updated with link to CmdArgs docs.

--- Main.hs
+++ Main.hs
@@ -1,9 +1,19 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module Main where
+
+import System.Console.CmdArgs
 
 import Beta
 import HmmPlus
 
-main = do (header, hmm, md) <- parse "test.hmm+"
+data SmurfArgs = SmurfArgs { hmmPlusFile :: FilePath }
+  deriving (Show, Data, Typeable)
+
+smurfargs = SmurfArgs { hmmPlusFile = def &= typ "HMM Plus file" &= argPos 0 }
+
+main = do sargs <- cmdArgs smurfargs
+          (header, hmm, md) <- parse $ hmmPlusFile sargs
           putStrLn $ show $ getBetaStrands header
           
 
