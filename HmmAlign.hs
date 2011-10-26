a2a5605c76cb98774a92d1e04140cae78d613df5
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2011-10-26 10:02:45 -0400

    i thought i committed this already...

--- HmmAlign.hs
+++ HmmAlign.hs
@@ -1,2 +1,11 @@
 module HmmAlign where
 
+import Data.Array
+
+import HmmPlus
+
+type QuerySequence = Array Int Char
+type Score = Int
+
+hmmAlign :: QuerySequence -> HMM -> Score
+
--- Main.hs
+++ Main.hs
@@ -5,6 +5,7 @@ module Main where
 import System.Console.CmdArgs
 
 import Beta
+import HmmAlign
 import HmmPlus
 
 data SmurfArgs = SmurfArgs { hmmPlusFile :: FilePath }
@@ -12,8 +13,12 @@ data SmurfArgs = SmurfArgs { hmmPlusFile :: FilePath }
 
 smurfargs = SmurfArgs { hmmPlusFile = def &= typ "HMM Plus file" &= argPos 0 }
 
+-- to be removed
+querySeq = listArray (1, 10) "ADBEHAQITP"
+
 main = do sargs <- cmdArgs smurfargs
           (header, hmm, md) <- parse $ hmmPlusFile sargs
+          putStrln $ show $ hmmAlign querySeq hmm
           putStrLn $ show $ getBetaStrands header
           
 
