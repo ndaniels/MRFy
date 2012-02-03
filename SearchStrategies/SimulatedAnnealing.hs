55eda8e5d11cd9f3a43d2c0239de3a3822e6ecd4
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-02-03 14:20:34 -0500

    slightly nicer accept function

--- SearchStrategies/SimulatedAnnealing.hs
+++ SearchStrategies/SimulatedAnnealing.hs
@@ -19,6 +19,10 @@ ss = SearchStrategy { accept = accept'
                     , initialize = initialize RHC.ss
                     }
 
+initialTemperature = 1000.0
+coolingFactor = 0.99
+boltzmannConstant = 1.0
+
 accept' :: Seed -> [Score] -> Age -> Bool
 accept' _ [] _ = error "go away"
 accept' _ [s1] _ = True
@@ -26,9 +30,7 @@ accept' seed (s1:s2:scores) age = boltzmann s1 s2 >= (p :: Double)
   where (p, gen) = random (mkStdGen seed)
     
         boltzmann :: Score -> Score -> Double
-        boltzmann s1 s2 = exp ((-(s1 - s2)) / (k * temperature))
+        boltzmann s1 s2 = exp ((-(s1 - s2)) / (boltzmannConstant * temperature))
 
-        k = 1.0
-        temperature = (0.99 ^^ age) * initialTemperature
-        initialTemperature = 1000.0
+        temperature = (coolingFactor ^^ age) * initialTemperature
 
