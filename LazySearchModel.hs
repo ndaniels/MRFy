c2fcdce573ba0d1c73eefe7fd78b6651b12b8b9c
Norman Ramsey <nr@cs.tufts.edu>
2012-06-02 16:19:10 +0200

    first cut at a very lazy search

--- LazySearchModel.hs
+++ LazySearchModel.hs
@@ -22,6 +22,9 @@ class RandomStream r where
   takeSeed r = (s, r1)
     where (r0, r1) = splitRand r
           s : _    = listRands r0
+  splitRand3 :: r -> (r, r, r)
+  splitRand3 r = let { (r0, r') = splitRand r; (r1, r2) = splitRand r' }
+                 in  (r0, r1, r2)
 
 -- @ start scoredecl.tex
 type Scorer placement = placement -> Scored placement
@@ -34,6 +37,7 @@ data NextState = SiblingNext | ChildNext
 data Approval = Accepted | Rejected
 type ScoredPopulation a = [Scored a]
 -- is there a better name for seed?
+{-
 class SearchStrategy s a where
   gen0    :: s -> Seed -> ScoredPopulation a
   nextGen :: s -> Seed -> ScoredPopulation a -> ScoredPopulation a
@@ -41,55 +45,63 @@ class SearchStrategy s a where
   children s r a = map (flip (nextGen s) a) (listRands r)
   approval :: s -> Seed -> History a -> Age -> Approval
 -- @ end strategy.tex
-{- 
+-}
 data SearchStrategy placement = 
  SS { gen0    :: Seed -> ScoredPopulation placement
     , nextGen :: Seed -> ScoredPopulation placement
                       -> ScoredPopulation placement
-    , approval :: Seed -> History placement -> Age -> Approval
+    , toApproved :: [Seed] -> Age
+                 -> [ScoredPopulation placement]
+                 -> [ScoredPopulation placement]
+       -- ^ @toApproved s age solns@ returns the smallest prefix
+       -- of @solns@ that contains an approved solution
     }
--}
 
+type SearchStop a = [Aged (ScoredPopulation a)] -> (Scored a, History a)
+
+children :: RandomStream r
+         => SearchStrategy a
+         -> r
+         -> ScoredPopulation a
+         -> [ScoredPopulation a]
+children ss r pop = map (flip (nextGen ss) pop) (listRands r)
+
+approvedPops :: RandomStream r
+             => SearchStrategy a
+             -> r
+             -> Age
+             -> ScoredPopulation a
+             -> [Aged (ScoredPopulation a)]
+approvedPops ss r age startPop =
+    Aged startPop age : approvedPops ss r0 (age + length kids) (last kids)
+  where kids = toApproved ss (listRands r1) age $ children ss r2 startPop
+        (r0, r1, r2) = splitRand3 r
+
+data Aged a = Aged a Age
+instance Functor Aged where
+  fmap f (Aged a age) = Aged (f a) age
   
-
-
-{-
 --------------------------------------------------------
 -- @ start search.tex
-search :: forall placement, r
+search :: forall placement r
         . RandomStream r
-       => SearchStrategy placement 
-       -> [(Scored placement, History placement)]
-search strat rand = runFrom seeds firstGen (History []) 0
- where
-  (s0, rand') = takeSeed
-  
-  firstGen = gen0 strat s0
-  runFrom :: r -> ScoredPopulation placement -> History placement
-          -> Age -> [(Scored placement, History placement)]
-  runFrom rand oldPop oldHist age =
-    let children = map (flip (nextGen strat)) (listRands r1) oldPop
-        trialHist = (winner, age) `hcons` oldHist
-        winner = minimum trialPop
-        (newPop, newHist) =
-          if accept strat s2 trialHist age then
-            (trialPop, trialHist)
-          else
-            (oldPop, oldHist)
-    in  if quit strat newHist age then -- TODO quit must change: consider best-ever, convergence
-          (fst $ hmin newHist, newHist) 
-        else
-          runFrom seeds newPop newHist (age + 1)
+       => SearchStrategy placement
+       -> SearchStop placement
+       -> r
+       -> (Scored placement, History placement)
+search strat finish rand = (finish . approvedPops strat r 0 . gen0 strat) s0
+ where (s0, r) = takeSeed rand
 -- @ end search.tex
 
 -- TODO keep a Scored (Age, Placement) to support Simulated Annealing
 -- otherwise, need an out of band "best ever" updated at every step
 -- this is also necessary to prevent SimAn from thinking it's improving
 -- when in fact it isn't.
--}
+
 
 data History placement = History [(Scored placement, Age)]
   deriving (Show, Eq, Ord)
+  -- XXX TODO 
 hmin :: History placement -> (Scored placement, Age)
 hmin (History as) = minimum as
 hcons :: (Scored placement, Age) -> History placement -> History placement
