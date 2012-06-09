eb87b2c82b12deaaae11e38caea1f6b90c9486d7
Norman Ramsey <nr@cs.tufts.edu>
2012-06-09 12:23:31 +0200

    more variations on randomly mutating a placement

--- SearchStrategies/RandomHillClimb.hs
+++ SearchStrategies/RandomHillClimb.hs
@@ -58,7 +58,9 @@ mutate _ query betas scorer (Scored oldp _) =
 -- strand is the adjacent beta strand; where there is no adjacent beta
 -- strand, the bound is 0 on the left and @n@ on the right.  The representation
 -- is a list @betas@ of beta strands and a list @oldp@ of their leftmost positions.
-randomizePlacement
+--
+-- precondition: length betas == length oldp && maxRight >= sum betas
+randomizePlacement 
   :: RandomGen r => [BetaStrand] -> Placement -> Int -> Rand r Placement
 randomizePlacement betas oldp maxRight = shiftFrom 0 0
   where shiftFrom i leftBound =
@@ -69,7 +71,29 @@ randomizePlacement betas oldp maxRight = shiftFrom 0 0
             where width = len (betas !! i)
                   rightBound = if succ i < length oldp then oldp !! succ i
                                else maxRight
-                                                         
+
+-- here's another version that passes a list of right bounds
+randomizePlacement' 
+  :: RandomGen r => [BetaStrand] -> Placement -> Int -> Rand r Placement
+randomizePlacement' betas oldp maxRight = shiftFrom betas 0 (tail oldp ++ [maxRight])
+  where shiftFrom (beta:betas) leftBound (rightBound:nextBounds) = do
+          g  <- getRandomR (leftBound, rightBound - len beta)
+          gs <- shiftFrom betas (g + len beta) nextBounds
+          return $ g : gs
+        shiftFrom [] _ [] = return []
+        shiftFrom _ _ _ = error "Norman blew the shift"
+
+randomizePlacementNoAllocation
+  :: RandomGen r => [BetaStrand] -> Placement -> Int -> Rand r Placement
+randomizePlacementNoAllocation betas oldp maxRight = shiftFrom betas 0 (tail oldp)
+  where shiftFrom (beta:betas) leftBound rights = do
+          g  <- getRandomR (leftBound, rightBound - len beta)
+          gs <- shiftFrom betas (g + len beta) nextBounds
+          return $ g : gs
+            where (rightBound, nextBounds) =
+                    case rights of []     -> (maxRight, error "shift bounds")
+                                   (b:bs) -> (b, bs)
+        shiftFrom [] _ _ = return []
 
 rightBound :: Placement -> Int -> QuerySequence -> Int
 rightBound oldp i query = if succ i < length oldp then oldp !! succ i
