5511de9d00b9e3f1ccc3852d7f988ca78afa8582
Norman Ramsey <nr@cs.tufts.edu>
2012-06-24 16:47:23 +0200

    cleaned up and documented Perturb.hs

--- Perturb.hs
+++ Perturb.hs
@@ -20,21 +20,9 @@ type SSeq = [State]
 type BSeq = [Block State]
 type Pred = State -> Bool
 
-newtype Plan7 = Plan7 SSeq deriving Show
+------------------------------------------------------------------------------
+-- PERTURBATIONS PART I: PURE MOVEMENT
 
-unPlan7 :: Plan7 -> [HMMState]
-unPlan7 (Plan7 states) = states
-
-isPlan7Prop (Plan7 states) = isPlan7 states
-
-instance Arbitrary Plan7 where
-  shrink (Plan7 states) = map Plan7 $ filter isPlan7 $ shrink states
-  arbitrary = fmap Plan7 $ sized $ \n ->
-                take <$> choose (0,n) <*> procededBy (const True)
-    where procededBy ok = do
-            next <- elements $ filter ok [Mat, Ins, Del]
-            rest <- procededBy (canFollow next)
-            return $ next : rest
 
 -- | @rightMovers p bs@ returns a list of all the sequences that can be
 -- obtained by taking the first element of @bs@ and moving it to the right,
@@ -76,18 +64,23 @@ rightMovesBefore ok s (Block s' n : bs)
 rightMovesBefore ok s [] = if ok s then [[Block s 1]] else []
 
 
+-- | @canFollow s s'@ tells whether one state can follow another 
+-- in the Plan7 scheme.  This predicate is symmetric:
+-- order doesn't matter.
 canFollow Del Ins = False
 canFollow Ins Del = False
 canFollow _   _   = True
 
 canMix s1 s2 = canFollow s1 s2 && canFollow s2 s1
 
-rightMoversStates, leftMoversStates :: SSeq -> [SSeq]
-rightMoversStates = map unblockify . rightMovers (const True) . blockify
-leftMoversStates  = map unblockify . leftMovers  (const True) . blockify
-
-rightMovesIntoStates :: State -> SSeq -> [SSeq]
-rightMovesIntoStates s = map unblockify . rightMovesInto (const True) s . blockify
+-- | @allMovers blocks@ returns sequences that can be obtained
+-- by allowing any state in any block to move left or right.
+-- (Left movement is obtained by right movement of a reversed list.)
+-- The sequences are carefully ordered by distance moved.
+-- The first element contains all results obtained by a move 
+-- of size 1, then the next element contains results of size 2, and so on.
+-- N.B. function @withStates allMovers@ will do something similar with
+-- a sequence of states.
 
 allMovers :: BSeq -> [[BSeq]]
 allMovers bs = roundRobin $ go [] bs
@@ -99,10 +92,33 @@ allMovers bs = roundRobin $ go [] bs
         okAfter [] = const True
         okAfter (b : _) = canFollow (state b)
 
+-- @ @rejoin xs' ys == reverse xs ++ ys@
+-- XXX TODO somebody should try both alternative with ghc and see if this 
+-- makes a difference
 rejoin :: [a] -> [a] -> [a]
 rejoin xs' ys = foldl (flip (:)) ys xs'
   -- using fold to hope for list fusion
 
+-- | @roundRobin@ takes a list of queues and returns a list of lists
+-- obtained by taking the head of each queue in turn.  We keep taking
+-- values until all queues are empty, or if any queue is infinite, forever.
+--
+-- RoundRobin might be quite similar to List.transpose, but we don't
+-- know what guarantees @transpose@ provides for irregular lists.
+
+roundRobin :: [[a]] -> [[a]]
+roundRobin [] = []
+roundRobin queues = let (heads, tails) = peel queues
+                    in  heads : roundRobin tails
+  where peel = unzip . catMaybes . map dequeue
+        dequeue [] = Nothing
+        dequeue (a:as) = Just (a, as)
+
+-----------------------------------
+-- Lifting block computations to state computations
+
+-- | Combinators for transforming block operations to sequence operations.
+-- The tags help ensure we got the distance stuff approximately right.
 withStates       :: (BSeq -> [[BSeq]]) -> (SSeq -> [SSeq])
 withTaggedStates :: (BSeq -> [[BSeq]]) -> (SSeq -> [(SSeq, Int)])
 
@@ -112,16 +128,18 @@ withTaggedStates movers =
   where tag n bs = map (\b -> (b,n)) bs
         leftMap f (x, y) = (f x, y)
         
-roundRobin :: [[a]] -> [[a]]
-roundRobin [] = []
-roundRobin queues = let (heads, tails) = peel queues
-                    in  heads : roundRobin tails
-  where peel = unzip . catMaybes . map dequeue
-        dequeue [] = Nothing
-        dequeue (a:as) = Just (a, as)
+
 
 ------------------------------------
 -- debugging code
+
+
+rightMoversStates, leftMoversStates :: SSeq -> [SSeq]
+rightMoversStates = map unblockify . rightMovers (const True) . blockify
+leftMoversStates  = map unblockify . leftMovers  (const True) . blockify
+
+rightMovesIntoStates :: State -> SSeq -> [SSeq]
+rightMovesIntoStates s = map unblockify . rightMovesInto (const True) s . blockify
 
 allRightMoversStates :: SSeq -> [(SSeq, Int)]
 allRightMoversStates = map (mapLeft unblockify) . concat . allRightMovers . blockify
@@ -136,48 +154,63 @@ allRightMovers bs = roundRobin $ go [] bs
         okAfter [] = const True
         okAfter (b : _) = canFollow (state b)
 
+------------------------------------------------------------------------------
+-- PERTURBATIONS PART II: PARTICLE DECAY FOLLOWED BY MOVEMENT
+
+-- | @decyMovers blocks@ returns sequences that can be obtained
+-- by allowing any Match state in any block to decay into an I/D or D/I pair
+-- of 'ions'.  The ions then move left or right as permitted by the
+-- plan7 invariants.  (At least one ion must move, otherwise we have
+-- adjacent ions.)
+
 decayMovers :: BSeq -> [[BSeq]]
 decayMovers bs = map concat $ roundRobin $ go [] bs
   where
     go :: BSeq -> BSeq -> [[[BSeq]]]
     go left' [] = []
-    go left' (b : right) = decayB b : go (b : left') right
-      where decayB (Block Mat n) =
+    go left' (b : right) = decayB b $ go (b : left') right
+      where decayB (Block Mat n) tail =
               [ block | i <- [1..n], ions <- [(Ins,Del), (Del,Ins)]
-                      , block <- split (i-1) (n-i) ions ]
-            decayB _ = []
+                      , block <- split (i-1) (n-i) ions ] : tail
+            decayB _ tail = tail
+
             split :: Int -> Int -> (State, State) -> [[BSeq]]
             split k_l k_r (plus, minus) =
               map catMaybes $ diagonals canJoin newLeft' newRight
                 where newLeft' = newSide plus k_l left'
                       newRight = newSide minus k_r right
-newSide ion n_matches rest =
-  addIon ion tail `consMaybe` rightMovesInto (const True) ion tail
-    where tail = addBlock Mat n_matches rest
 
 
-consMaybe Nothing xs = xs
-consMaybe (Just x) xs = x : xs
+    -- the functions below are all definable at top level,
+    -- but they are auxiliary functions for decayMovers
 
-addIon :: State -> BSeq -> Maybe BSeq
-addIon s [] = Just [Block s 1]
-addIon s (Block s' n : bs)
-  | s == s' = Just (Block s (succ n) : bs)
-  | canFollow s s' = Just (Block s 1 : Block s' n : bs)
-  | otherwise = Nothing
+    newSide ion n_matches rest =
+      addIon ion tail `consMaybe` rightMovesInto (const True) ion tail
+        where tail = addBlock Mat n_matches rest
 
-addBlock :: State -> Int -> BSeq -> BSeq
-addBlock _ 0 bs = bs
-addBlock s n (Block s' n' : bs)
-  | s == s' = Block s (n+n') : bs
-addBlock s n bs = Block s n : bs
+    consMaybe Nothing xs = xs
+    consMaybe (Just x) xs = x : xs
 
-canJoin (Block s _ : _) (Block s' _ : _)
-  | not (canFollow s s') = Nothing
-canJoin bs bs' = Just (bs `rejoin` bs')
+    addIon :: State -> BSeq -> Maybe BSeq
+    addIon s [] = Just [Block s 1]
+    addIon s (Block s' n : bs)
+      | s == s' = Just (Block s (succ n) : bs)
+      | canFollow s s' = Just (Block s 1 : Block s' n : bs)
+      | otherwise = Nothing
+
+    addBlock :: State -> Int -> BSeq -> BSeq
+    addBlock _ 0 bs = bs
+    addBlock s n (Block s' n' : bs)
+      | s == s' = Block s (n+n') : bs
+    addBlock s n bs = Block s n : bs
+
+    canJoin (Block s _ : _) (Block s' _ : _)
+      | not (canFollow s s') = Nothing
+    canJoin bs bs' = Just (bs `rejoin` bs')
     
 ----------------------------------------------------------
 --
+-- code to get distances right when summing left and right moves
         
 diagonals :: (a -> b -> c) -> [a] -> [b] -> [[c]]
 diagonals f (x0:xs) (y0:ys) =
@@ -200,17 +233,37 @@ diagonalsCount (Positive n') (Positive m') =
     where m = min m' 300
           n = min m' 300
  
-------------------------------------
--- properties
+-----------------------------------------------------------------------------------
+-- TESTINB PERTURBATIONS AND RELATED CODE
+
+
+newtype Plan7 = Plan7 SSeq deriving Show
+
+instance Arbitrary Plan7 where
+  shrink (Plan7 states) = map Plan7 $ filter isPlan7 $ shrink states
+  arbitrary = fmap Plan7 $ sized $ \n ->
+                take <$> choose (0,n) <*> procededBy (const True)
+    where procededBy ok = do
+            next <- elements $ filter ok [Mat, Ins, Del]
+            rest <- procededBy (canFollow next)
+            return $ next : rest
+
+
 
 rightMoversPermutesProp (Plan7 ss) = all match (rightMoversStates ss)
   where match ss' = sort ss' == sort ss -- big hammer
+
+-- | Predicate @<==>@ judges the equivalence of two Plan7 sequences.
+-- N.B. It does not guarantee both lists are Plan7, just that
+-- they both agree on whether they are Plan7.
 
 (<==>) :: SSeq -> SSeq -> Bool
 ss <==> ss' = agree nodeCount && agree residueCount && agree isPlan7
   where agree f = f ss == f ss'
 
 preservesInvariants f (Plan7 ss) = all (<==> ss) $ take 100000 $ f ss
+
+isPlan7Prop (Plan7 states) = isPlan7 states
 
 perturbProps :: [(String, Property)]
 perturbProps = [ ("diagonal", property diagonalProp)
