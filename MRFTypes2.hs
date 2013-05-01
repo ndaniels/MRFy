7288ab0799177d5d7773b7fa79005395d3fa2e4e
Andrew Gallant <Andrew.Gallant@tufts.edu>
2013-05-01 15:25:57 -0400

    MRFTypes2 is back to good.

--- MRFTypes2.hs
+++ MRFTypes2.hs
@@ -5,6 +5,7 @@ import Debug.Trace
 
 import qualified Data.Array as A
 import qualified Data.Vector.Unboxed as U
+import Text.Printf (printf)
 import Test.QuickCheck
 import Test.QuickCheck.Arbitrary
 import Test.QuickCheck.Gen
@@ -14,11 +15,15 @@ import qualified HMMArby as Arby
 import qualified MRFTypes as T
 import qualified Score as S
 
-newtype HMM = HMM (BeginNode, A.Array Int MiddleNode, EndNode)
-              deriving (Show, Eq)
+newtype HMM = HMM (BeginNode, A.Array Int MiddleNode)
+              deriving (Eq)
+
+instance Show HMM where
+  show (HMM (beg, mids)) = printf "HMM\n%s\n%s\n"
+                                  (show beg) (show $ A.elems mids)
 
 numNodes :: HMM -> Int
-numNodes (HMM (hmmBegin, mids, hmmEnd)) = 2 + (end - start + 1)
+numNodes (HMM (_, mids)) = 1 + (end - start + 1)
   where (start, end) = A.bounds (mids)
 
 data BeginNode = BeginNode { bmate :: T.EProbs
@@ -29,7 +34,13 @@ data BeginNode = BeginNode { bmate :: T.EProbs
                            , b_i_m :: T.TProb
                            , b_i_i :: T.TProb
                            }
-                 deriving (Show, Eq)
+                 deriving (Eq)
+
+instance Show BeginNode where
+  show b = printf "BEGIN (inse %f, trans %f)"
+                  (S.unScore $ U.sum $ binse b) (transSum)
+    where transSum = sum $ map (S.unScore . T.logProbability) probs
+          probs = [b_m_m b, b_m_i b, b_m_d b, b_i_m b, b_i_i b]
 
 
 -- We are choosing to represent the last "real" node as an end
@@ -41,7 +52,8 @@ data BeginNode = BeginNode { bmate :: T.EProbs
 --
 --
 --
-data EndNode = EndNode.-- always in match state; no transitions out
+data EndNode = EndNode -- always in match state; no transitions out
+               deriving (Eq, Show)
 
 data MiddleNode = MiddleNode { mate :: T.EProbs
                              , inse :: T.EProbs
@@ -53,27 +65,32 @@ data MiddleNode = MiddleNode { mate :: T.EProbs
                              , d_m :: T.TProb
                              , d_d :: T.TProb
                              }
-                 deriving (Show, Eq)
+                 deriving (Eq)
+
+instance Show MiddleNode where
+  show n = printf "MIDDLE (mate %f, inse %f, trans %f)"
+                  (S.unScore $ U.sum $ mate n)
+                  (S.unScore $ U.sum $ inse n)
+                  (transSum)
+    where transSum = sum $ map (S.unScore . T.logProbability) probs
+          probs = [m_m n, m_i n, m_d n, i_m n, i_i n, d_m n, d_d n]
 
 asBegin :: MiddleNode -> BeginNode
 asBegin n = BeginNode (mate n) (inse n) (m_m n) (m_i n) (m_d n) (i_m n) (i_i n)
 
-asEnd :: MiddleNode -> EndNode
-asEnd n = EndNode (mate n) (inse n) (m_m n) (m_i n) (i_m n) (i_i n) (d_m n)
-
-data Seq a = Seq { get :: Int -> a, seqLength :: Int } 
+-- data Seq a = Seq { get :: Int -> a, seqLength :: Int }  
 
 data Model = Model { begin :: BeginNode
                    , middle :: Int -> MiddleNode
                    , end :: EndNode
-                   , size :: Int
+                   , midSize :: Int
                    }
 
 instance Eq Model where
   m1 == m2 = modelList m1 == modelList m2
 
 instance Show Model where
-  show m = "(MODEL: size = " ++ show (size m) ++ ") "
+  show m = "(MODEL: size = " ++ show (midSize m) ++ ") "
 
 type Interval = (Int, Int)
 
@@ -81,36 +98,23 @@ type Interval = (Int, Int)
 -- can have a single node (just the `BeginNode`), while an `HMM` needs
 -- at least a `BeginNode` and an `EndNode`.
 modelHMM :: Model -> HMM
-modelHMM = listHMM . modelList
-  where listHMM (Just beg, mids, Just end) = HMM (beg, listArray mids, end)
+modelHMM mod = HMM (beg, listArray mids)
+  where (beg, mids, _) = modelList mod
 
-modelList :: Model -> (Maybe BeginNode, [MiddleNode], Maybe EndNode)
-modelList = mfoldr fbeg fmid fend (Nothing, [], Nothing)
-  where fbeg n (_, mids, end) = (Just n, mids, end)
-        fmid n (beg, mids, end) = (beg, n:mids, end)
-        fend n (beg, mids, _) = (beg, mids, Just n)
+modelList :: Model -> (BeginNode, [MiddleNode], EndNode)
+modelList mod = (begin mod, reverse $ midsList (midSize mod - 1), end mod)
+  where midsList (-1) = []
+        midsList   n  = mid n : midsList (n - 1)
 
-  -> Model
-mfoldr
-  :: (BeginNode -> a -> a)
-  -> (MiddleNode -> a -> a)
-  -> (EndNode -> a -> a)
-  -> (Model -> a -> a)
-mfoldr bf mf ef init mod
-  | size mod == 0 = init
-  | size mod == 1 = bf (begin mod) init
-  | otherwise = bf (begin mod) (mfold' 0)
-    where mfold' n
-            | n == size mod - 2 = ef (end mod)       init
-            | otherwise         = mf (middle mod n) (mfold' (n + 1))
+        mid = middle mod
 
 slice :: HMM -> Interval -> Model
-slice hmm@(HMM (hmmBegin, mids, hmmEnd)) (start, len) =
-  Model begNode mid endNode len
+slice hmm@(HMM (hmmBegin, mids)) (start, len) =
+  Model begNode mid EndNode (len - 1)
   where mid :: Int -> MiddleNode
         mid n
           | n < 0         = error "< oob"
-          | n >= len - 2  = error "> oob"
+          | n >= len - 1  = error "> oob"
           -- Gross. Indexing here must be offset by whether the
           -- begin node was the real begin node or if it was
           -- plucked from the middle nodes.
@@ -122,9 +126,6 @@ slice hmm@(HMM (hmmBegin, mids, hmmEnd)) (start, len) =
         begNode = if   start == 0
                   then hmmBegin
                   else asBegin (mids A.! (start - 1))
-        endNode = if   start + len == numNodes hmm 
-                  then hmmEnd
-                  else asEnd (mids A.! (start + len - 2))
 
 listArray :: [a] -> A.Array Int a
 listArray xs = A.listArray (0, length xs - 1) xs
@@ -146,11 +147,11 @@ randEProbs = do
           toScore f = if f == 0.0 then S.negLogZero else S.Score (- log f)
 
 instance Arbitrary HMM where
-  shrink (HMM (beg, mids, end)) = [HMM (beg, m, end) | m <- mids' ]
+  shrink (HMM (beg, mids)) = [HMM (beg, m) | m <- mids' ]
     where mids' = map listArray $ shrink $ A.elems mids
   arbitrary = do
     (beg, mids, end) <- arbitrary :: Gen (BeginNode, [MiddleNode], EndNode)
-    return $ HMM (beg, listArray mids, end)
+    return $ HMM (beg, listArray mids)
 
 instance Arbitrary BeginNode where
   arbitrary = do
@@ -161,12 +162,7 @@ instance Arbitrary BeginNode where
     return $ BeginNode mate inse mm mi md im ii
 
 instance Arbitrary EndNode where
-  arbitrary = do
-    mate <- randEProbs
-    inse <- randEProbs
-    (mm, mi) <- arbitrary :: Gen (T.TProb, T.TProb)
-    (im, ii, dm) <- arbitrary :: Gen (T.TProb, T.TProb, T.TProb)
-    return $ EndNode mate inse mm mi im ii dm
+  arbitrary = return EndNode
 
 instance Arbitrary MiddleNode where
   arbitrary = do
@@ -201,10 +197,8 @@ instance Arbitrary Sliceable where
 -- len == |slice (start, len) hmm|
 propSliceLen :: Sliceable -> Bool
 propSliceLen (Sliceable ((start, len), hmm)) =
-  len == count
-  where count = mfoldr inc inc inc 0 mod
-        mod = slice hmm (start, len)
-        inc _ n = n + 1
+  len == 1 + midSize mod
+  where mod = slice hmm (start, len)
 
 -- slice hmm = (slice . toHMM . slice) hmm
 propInvertible :: Sliceable -> Bool
@@ -223,5 +217,8 @@ propInverse hmm = hmm == hmm'
 -- |hmm| == |slice (0, |hmm|) hmm|
 -- subsumed by `propSliceLen` i think
 propSliceLenFull :: HMM -> Bool
-propSliceLenFull hmm = numNodes hmm == size (slice hmm (0, numNodes hmm))
+propSliceLenFull hmm = numNodes hmm == 1 + midSize (slice hmm (0, numNodes hmm))
+
+traceid :: (Show a) => a -> a
+traceid x = trace ("[[TRACE: " ++ show x ++ "]]") x
 
